f4030b30331d422baa547d7324ac18fa1c228b32
rudaina-hoorain <rudainahhoorain@gmail.cm>
2026-08-13 22:11:21 +0530

    Testt auto deployment

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 8080;
 
 app.get('/', (req, res) => {
-  res.send('🚀 v2 - Auto-deployed via Azure CI/CD Pipeline!');
+  res.send('🚀 v2 - Auto-deployed via Azure CI/CD Pipeline');
 });
 
 app.listen(port, () => {
