036c213bfcc2526a87245bf5a050d1373c779e25
rudaina-hoorain <rudainahhoorain@gmail.cm>
2026-08-13 22:47:47 +0530

    index file changed

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ app.get('/', (req, res) => {
   res.send('🚀 v2 - Auto-deployed via Azure CI/CD Pipeline');
 });
 
-app.listen(port, () => {
+app.listen(port, '0.0.0.0', () => {
   console.log(`App running on port ${port}`);
 });
